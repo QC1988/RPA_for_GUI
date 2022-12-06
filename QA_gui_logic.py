2e82cc56dd2e7a0949ccfe0898dd62bed89ad632
QC1988 <50434083+QC1988@users.noreply.github.com>
2022-12-06 13:58:02 +0900

    Update QA_gui_logic.py

--- QA_gui_logic.py
+++ QA_gui_logic.py
@@ -4,14 +4,16 @@
 # from QA_gui_main import RMA_paras
 import os
 import wx
+import time
 from sys import path
+
+from wx.core import Sleep
 pwd = os.getcwd()
 father_path=os.path.abspath(os.path.dirname(pwd)+os.path.sep+".")
 path.insert(0, father_path)
 path.insert(0, father_path+'\\Create_RMA_List')
 
 import Create_RMA_list
-import write_ini
 import configparser
 import QA_gui_template
 
@@ -23,47 +25,76 @@ class myconf(configparser.ConfigParser):
 
 class LogicFrame(QA_gui_template.Frame):
 
-    #处理侧栏的点击事件
+    # handle the click event of the sidebar
     def Onclick_Ce(self,event):
         # CeSizer = wx.BoxSizer(wx.VERTICAL)
         if event.GetEventObject() == self.button_RMA:
-            print("RMA")
-            # 如果需要显示的地方存在其他面板，删除
-            if self.panel_inter2:
-                #  self.panel_inter2.Destroy()
-                 self.panel_inter2.Destroy()
-            if self.panel_inter3:
-                # self.panel_inter3.Destroy()
-                self.panel_inter3.Destroy()
-            if not self.panel_inter1 :
-                self.InitInterPanel()
+            print("RMA button was pressed.")
+            # if there is some other panel exsit in the display area then delete it
+            if self.panel_inter1:
+                print("RMA panel now.")
+                pass
+
+            else:
+                if self.panel_inter2:
+                    self.panel_inter2.Destroy()
+                    print("destory inter1")
+                if self.panel_inter3:
+                    self.panel_inter3.Destroy()
+                    print("destory inter1")
+                self.drawRMApanel()
+                print("Changed to RMA panel.")
                 
-                # .Prepend(self.panel_inter1, wx.EXPAND| wx.ALL, 5)
-                self.Layout()
-                # self.panel_inter1 = wx.Panel(self, pos=(200, 0), size=(700, 200))
-                # wx.StaticText(self.panel_inter1, label="RMA list name", pos=(20, 20))
-                # self.RMA_name = wx.TextCtrl(self.panel_inter1, pos=(120,15))  
-                # self.RMA_process_btn=wx.Button(self.panel_inter1,label=u'実行',pos=(100,100),size=(50,30))
-                # self.panel_inter1.Bind(wx.EVT_BUTTON,self.OnclickRMAbutton,self.RMA_process_btn)
-            #     if(denglu_flag == False):
-            #         # self.InitButton()
-            #         print("******")
-            #     else:
-            #         if not self.panel_inter1_1:
-            #             self.panel_inter1_1 = wx.Panel(self, pos=(400, 150), size=(300, 300))
-            #             wx.StaticText(self.panel_inter1_1, label="欢迎登陆", pos=(130, 150))
+
+            # if self.panel_inter2:
+            #     #  self.panel_inter2.Destroy()
+            #      self.panel_inter2.Destroy()
+            #      self.drawRMApanel()
+            #      print("panel_inter2")
+            # if self.panel_inter3:
+            #     # self.panel_inter3.Destroy()
+            #     self.panel_inter3.Destroy()
+            #     self.drawRMApanel()
+            #     print("panel_inter3")
+            # if not self.panel_inter1 :
+            #     print("panel_inter1")
+            #     # LogicFrame.Destory()
+            #     # self.InitInterPanel()
+            #     self.drawRMApanel()
+                
+            #     # .Prepend(self.panel_inter1, wx.EXPAND| wx.ALL, 5)
+            #     self.Layout()
+            #     # self.panel_inter1 = wx.Panel(self, pos=(200, 0), size=(700, 200))
+            #     # wx.StaticText(self.panel_inter1, label="RMA list name", pos=(20, 20))
+            #     # self.RMA_name = wx.TextCtrl(self.panel_inter1, pos=(120,15))  
+            #     # self.RMA_process_btn=wx.Button(self.panel_inter1,label=u'process',pos=(100,100),size=(50,30))
+            #     # self.panel_inter1.Bind(wx.EVT_BUTTON,self.OnclickRMAbutton,self.RMA_process_btn)
+            # #     if(denglu_flag == False):
+            # #         # self.InitButton()
+            # #         print("******")
+            # #     else:
+            # #         if not self.panel_inter1_1:
+            # #             self.panel_inter1_1 = wx.Panel(self, pos=(400, 150), size=(300, 300))
+            # #             wx.StaticText(self.panel_inter1_1, label="Welcome.", pos=(130, 150))
 
         if event.GetEventObject()==self.button_NEP:
-            # 如果需要显示的地方存在其他面板，删除
-            print("NEP")
-            if self.panel_inter1:
-                self.panel_inter1.Destroy()
-            # if self.panel_inter1_1:
-            #     self.panel_inter1_1.Destroy()
-            if self.panel_inter3:
-                self.panel_inter3.Destroy()
-            if not self.panel_inter2 :
+            # if there is some other panel exsit in the display area then delete it
+            print("NEP was pressed.")
+            if self.panel_inter2:
+
+                print("NEP panel now.")
+                pass
+
+            else :
+                if self.panel_inter1:
+                    self.panel_inter1.Destroy()
+                    print("destory inter1")
+                if self.panel_inter3:
+                    self.panel_inter3.Destroy()
+                    print("destory inter3")                    
                 self.drawNEPpanel()
+                print("Change to RMA panel.")
+                
 
         if event.GetEventObject() == self.button_SBF:
             print("SBF")
@@ -74,6 +105,7 @@ class LogicFrame(QA_gui_template.Frame):
             if  self.panel_inter2 :
                 self.panel_inter2.Destroy()
             if not self.panel_inter3:
+                print("SBF button was pressed.")
                 self.drawSBFpanel()
 
     def GetRMAname(self):
@@ -85,18 +117,22 @@ class LogicFrame(QA_gui_template.Frame):
     def OnclickRMAbutton(self, event):
         conf = myconf()
         if os.path.exists(father_path+'\\config.ini'):
-            conf.read(father_path+'\\config.ini',encoding='utf-8')
-            RMA_paras = conf['paras_for_RMA']
+            # conf.read(father_path+'\\config.ini',encoding='utf-8')
+            # RMA_paras = conf['paras_for_RMA']
+            pass
         else:
             print("The config file was not found!")
             exit()
         if event.GetEventObject()==self._submit_btn:
-            RMA_paras['RMA_list_name'] = self.GetRMAname() + '.xlsx'
-            config_path_tmp = father_path +'\\config.ini'
+            # RMA_paras['RMA_list_name'] = self.GetRMAname() + '.xlsx'
+            RMA_paras_RMA_list_name = self.GetRMAname() + '.xlsx'
+            config_path_tmp = father_path + '\\config.ini'
             print(config_path_tmp)
-            write_ini.main(config_path_tmp, 'paras_for_RMA',RMA_paras)
-            print(RMA_paras['RMA_list_name'])
-            Create_RMA_list.main()
+            # write_ini.main(config_path_tmp, 'paras_for_RMA', RMA_paras)
+            # print(RMA_paras['RMA_list_name'])
+            print(RMA_paras_RMA_list_name)
+            # time.sleep(3)
+            Create_RMA_list.main(RMA_paras_RMA_list_name)
 
 
     # def buttonCebian1Click(self, event):
